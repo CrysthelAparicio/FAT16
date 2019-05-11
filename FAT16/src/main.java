6b91117626ab89bbdf3b290d1ec5678d54d456ef
CrysthelAparicio <apariciocrysthel@gmail.com>
2019-05-11 11:39:38 -0600

    Haciendo prueba de mi usuario de git

--- FAT16/src/main.java
+++ FAT16/src/main.java
@@ -8,23 +8,23 @@
  *
  * @author Crys
  */
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class main {
+
     // Tam en bytes
     final static int TAM_CLUSTER = 4096;
     final static int TAM_RAIZ = 16384;
     final static int TAM_FAT = 131072;
     final static int TAM_DIRENTRY = 32;
     final static int TAM_FATENTRY = 2;
-    
+
     //cantidad de entradas posibles
     final static int ENTRADASMAX_RAIZ = TAM_RAIZ / TAM_DIRENTRY;
     final static int CURRENTDIR_ENTRADASMAX = TAM_CLUSTER / TAM_DIRENTRY;
-    
+
     //otros
     static Scanner read = new Scanner(System.in);
     static String archiv_bin = "./proyect.bin";
@@ -37,12 +37,19 @@ public class main {
     final static byte ARCHIV = (byte) 32;
     final static byte SOLO_LECTURA = (byte) 1;
     final static byte ESCONDIDO = (byte) 2;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        boolean shell = true;
+        while (shell) {
+            System.out.print(current_dir + " : ");
+            String entrada = read.nextLine();
+            //Otra carpeta 
+            String[] comando = entrada.split(" ");
+
+        }
+
     }
-    
 }
